c4117fdbc3277f59b427071f51e43cb37ecd2119
yastar123 <eduj2164@gmail.com>
2025-07-08 00:20:50 +0700

    Final

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),]
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,          // atau sesuaikan dengan proses deploy-mu
+    allowedHosts: ['miniprojectfepplk2025.onrender.com']
+  }
 })
